c7d3ea5700e23062592828ad026eff3da47abb07
Sam Precious <samwdp@gmail.com>
2020-05-18 18:20:16 +0100

    Initial commit of source

---
+++ src/API/ShoppingPositioningSystem.MobileAppService.Service/TescoSearch.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ShoppingPositioningSystem.MobileAppService.Service
+{
+    public class TestSearch
+    {
+        private readonly IConfiguration _config;
+        private HttpClient _client;
+        public TestSearch(IConfiguration config)
+        {
+            _config = config;
+            _client = new HttpClient();
+        }
+
+        public async Task<string> GetSearch(string searchTerm)
+        {
+            _client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", _config["tescoAuthToken"]);
+            string uri = string.Format(_config["tescoSearchUrl"], searchTerm);
+
+            var response = await _client.GetAsync(uri);
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
